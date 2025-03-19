d55235f0caac53d00923eb428041a5825e94062a
Pablovv56 <vazquezverapablo@gmail.com>
2025-03-19 19:13:38 +0100

    Added ATE metric and improvements to topic sincronization, performance and overall precision

--- project_ws/install/location_controller/lib/python3.10/site-packages/location_controller/input_data_module.py
+++ project_ws/install/location_controller/lib/python3.10/site-packages/location_controller/input_data_module.py
@@ -2,10 +2,10 @@
 
 import rclpy
 import rclpy.serialization
+import logging
 import rosbag2_py
 import os
 import sys
-import struct
 
 from rclpy.node import Node
 from sensor_msgs.msg import PointCloud2
@@ -17,38 +17,98 @@ recordings_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../..
 # Adding the recordings path to python execution
 sys.path.append(recordings_path)
 
-# Setting recording path
-bag_path = os.path.abspath(os.path.join(recordings_path , "rosbag.db3"))
-
-pcl_topic = "/cloud_in"
-pose_topic = "/gt_odom"
+logger = logging.getLogger("InputData")
 
 class InputData (Node):
     
+    # Topic names
+    PCL_TOPIC = "/cloud_in"
+    POSE_TOPIC = "/gt_odom"
+    FRAME_ID = "map"
+    
+    # Setting recording path
+    BAG_PATH = os.path.abspath(os.path.join(recordings_path , "rosbag.db3"))
+    
     def __init__(self):
+        
+        """
+        Initializes the InputDataNode class.
+        This constructor sets up the ROS2 node, initializes publishers and subscribers, 
+        and prepares the rosbag reader for data processing. It also publishes the first 
+        pose and point cloud data upon initialization.
+        Attributes:
+            location_node_subscriber (Subscription): Subscriber to the "new_pose" topic, 
+                which triggers the `input_data_callback` method.
+            input_data_publisher_pcl (Publisher): Publisher for the "clean_pcl" topic, 
+                which provides the cleaned point cloud data.
+            input_data_publisher_pose (Publisher): Publisher for the "clean_pose" topic, 
+                which provides the cleaned pose data.
+            reader (rosbag2_py.SequentialReader): Reader for accessing data from the 
+                specified rosbag file.
+            temp_pose (PoseStamped or None): Temporary storage for the current pose.
+            index (int): Counter to track the current index of processed data.
+        """
 
         # Initialize node
         super().__init__("input_data_node")
         
         # Create subscription to /new_pose, triggers input_data_callback
         self.location_node_subscriber = self.create_subscription(PoseStamped, "new_pose", self.input_data_callback, 10)
+        
         # Create a publisher that will provide the clean pcl
         self.input_data_publisher_pcl = self.create_publisher(PointCloud2, 'clean_pcl', 10)
+        
         # Create a publisher that will provide the clean pose
         self.input_data_publisher_pose = self.create_publisher(PoseWithCovarianceStamped, 'clean_pose', 10)
         
         # Initialize the data that will be published
-        storage_options = rosbag2_py.StorageOptions(uri=bag_path, storage_id="sqlite3")
+        storage_options = rosbag2_py.StorageOptions(uri=self.BAG_PATH, storage_id="sqlite3")
         converter_options = rosbag2_py.ConverterOptions(input_serialization_format = "cdr", output_serialization_format = "cdr")
         self.reader = rosbag2_py.SequentialReader()
         self.reader.open(storage_options, converter_options)
         
-        # Publish the firsts poses and pcl
+        # Temp pose
+        self.temp_pose = None
+        self.index = 0
+        
+        # Publish the first pose and pcl
         self.input_data_callback(None)
 
-
+    #Function to call back when the poses are published
     def input_data_callback (self, msg):
         
+        """
+        Callback function to process input data messages from a recorded bag file.
+        This function reads messages from a bag file using a reader object and processes
+        them based on their topic. It handles two types of topics: PointCloud2 and 
+        PoseWithCovarianceStamped. The processed messages are published to corresponding 
+        ROS 2 topics.
+        Args:
+            msg: The incoming message (not used in this implementation, as the function 
+                 reads messages directly from the bag file).
+        Behavior:
+            - If the topic is a PointCloud2 message:
+                - Deserializes the message.
+                - Updates the frame ID to "map".
+                - Publishes the deserialized PointCloud2 message.
+                - Publishes the latest pose associated with the point cloud.
+                - Increments the message index.
+            - If the topic is a PoseWithCovarianceStamped message:
+                - Deserializes the message.
+                - Updates the frame ID to "map".
+                - Updates the timestamp with the current message index.
+                - Stores the pose temporarily for future use.
+                - Recursively continues reading the file until a PointCloud2 message is found.
+            - If the topic is neither of the above:
+                - Recursively continues reading the file until a PointCloud2 message is found.
+        Exceptions:
+            - Logs an error if deserialization or publishing fails for any message.
+        Note:
+            This function assumes that the reader object and publishers are properly 
+            initialized and that the FRAME_ID, PCL_TOPIC, and POSE_TOPIC constants are 
+            correctly defined.
+        """
+    
         # We prepare to publish the first pcd
         if self.reader.has_next():
             
@@ -56,33 +116,52 @@ class InputData (Node):
             topic, msg, t = self.reader.read_next()
             
             # Filter topics
-            if topic == pcl_topic:
+            if topic == self.PCL_TOPIC:
                 
                 # Try to deserialize the topic
                 try:
                     # Converts the message from bytes to PointCloud2
                     deserialized_msg = rclpy.serialization.deserialize_message(msg, PointCloud2)
                     
+                    # We change the frame id to make sure its "map"
+                    deserialized_msg.header.frame_id = self.FRAME_ID
+                    
+                    # Publish the point cloud
                     self.input_data_publisher_pcl.publish(deserialized_msg)
+                    
+                    # Publish the latest pose associated to the pcl
+                    self.input_data_publisher_pose.publish(self.temp_pose)                 
+                    
+                    # Increase the msg count
+                    self.index += 1
                 
                 except Exception as e:
-                    self.get_logger().error(f"Failed to deserialize or publish message: {e}")
+                    logger.error(f"Failed to deserialize or publish message: {e}")
                     
-            elif topic == pose_topic:
+            elif topic == self.POSE_TOPIC:
                 
                 # Try to deserialize the topic
                 try:
                     # Converts the message from bytes to PoseWithCovarianceStamped
                     deserialized_msg = rclpy.serialization.deserialize_message(msg, PoseWithCovarianceStamped)
- 
-                    self.input_data_publisher_pose.publish(deserialized_msg)
                     
+                    # We add an index to track poses
+                    deserialized_msg.header.stamp.sec = self.index
+                    
+                    # We change the frame id to make sure its "map"
+                    deserialized_msg.header.frame_id = self.FRAME_ID
+ 
+                    self.temp_pose = deserialized_msg
+                    # self.input_data_publisher_pose.publish(deserialized_msg)
+                    
+                    # Continue reading the file until we find the next PCL
                     self.input_data_callback(None)
                 
                 except Exception as e:
-                    self.get_logger().error(f"Failed to deserialize or publish message: {e}")
+                    logger.error(f"Failed to deserialize or publish message: {e}")
                 
             else: 
+                # Continue reading the file until we find the next PCL
                 self.input_data_callback(None)
                     
                 
--- project_ws/install/location_controller/lib/python3.10/site-packages/location_controller/location_module.py
+++ project_ws/install/location_controller/lib/python3.10/site-packages/location_controller/location_module.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 import rclpy
+import time
+import logging
 import numpy as np
 import copy as cp
 import open3d as o3d
 import tf_transformations as tf
-import time
 
 from location_controller.utils import draw_registration_result, pointcloud2_to_open3d
 from geometry_msgs.msg import PoseStamped
@@ -14,25 +15,45 @@ from rclpy.node import Node
 
 # Global parameters
 
-# Adjust fitting parameters
-voxel_size = 0.05
-threshold = 0.02
+# Configure logging
+logging.basicConfig(
+    level=logging.DEBUG,  # Set the logging level to DEBUG to capture all log messages
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  # Log format
+    handlers=[
+        logging.StreamHandler()  # Output logs to the console
+    ]
+)
+
+# Initialize logger
+logger = logging.getLogger("LocatioNode")
 
 class LocationNode (Node):
 
     # Class variables
     
-    # Last point cloud data recieved
-    actual_PCD = None
-    # Last pose data recieved calculated
-    actual_pose = np.identity(4)
-    
-    # Mean error
-    mean_error = 0
-    # Number of messages recieved
-    n_msg = 0
+    # Adjust fitting parameters
+    VOXEL_SIZE = 0.05
+    THRESHOLD = 0.01
+    VOXEL_REDUCTION_RATIO = 1
+
+    # Preprocess Point Cloud
+    NORMAL_FACTOR = 2
+    FEATURE_FACTOR = 5
 
     def __init__(self, input_topic : str):
+        
+        """
+        Initializes the localization node.
+        Args:
+            input_topic (str): The name of the input topic to subscribe to for PointCloud2 messages.
+        Attributes:
+            location_node_ (Subscription): Subscription to the specified input topic, triggering the pose_callback.
+            location_publisher_ (Publisher): Publisher for broadcasting the calculated pose as a PoseStamped message.
+            index (int): Counter for indexing received messages.
+            actual_PCD (PointCloud2 or None): Stores the most recent PointCloud2 data received.
+            actual_pose (numpy.ndarray): Stores the most recent calculated pose as a 4x4 transformation matrix.
+            mean_error (float): Tracks the mean error for debugging purposes (only initialized if debug mode is enabled).
+        """
 
         # Initialize node
         super().__init__("localization_node")
@@ -43,10 +64,57 @@ class LocationNode (Node):
         # Create a publisher that will provide the actual pose
         self.location_publisher_ = self.create_publisher(PoseStamped, 'new_pose', 10)
 
+        # Variable for indexing messages
+        self.index = 0
+    
+        # Last point cloud data recieved
+        self.actual_PCD = None
+        
+        # Last pose data recieved calculated
+        self.actual_pose = np.identity(4)
+        
+        if logger.isEnabledFor(logging.DEBUG):
+            # Mean error
+            self.mean_error = 0
+            
+        logger.info("Node Initialization Complete")
+
 
     def pose_callback (self, msg : PointCloud2):
+        
+        """
+        Callback function to process incoming PointCloud2 messages and perform point cloud registration.
+        This function handles the localization process by comparing the incoming point cloud data with 
+        the previously stored point cloud. It performs preprocessing, initial alignment, and iterative 
+        closest point (ICP) registration to compute the transformation between the two point clouds. 
+        The computed transformation is used to update the current pose of the system.
+        Args:
+            msg (PointCloud2): The incoming point cloud message.
+        Workflow:
+            1. If this is the first message, store the point cloud and publish the initial pose.
+            2. If this is not the first message:
+                - Preprocess the source and target point clouds (downsampling and feature extraction).
+                - Perform initial alignment using RANSAC or Fast Global Registration.
+                - Compute normals for the target point cloud.
+                - Perform ICP registration to refine the alignment.
+                - Update the current pose using the computed transformation.
+                - Publish the updated pose as a PoseStamped message.
+        Debugging:
+            - Logs the time taken for each step if the `debug` flag is enabled.
+            - Computes and logs the Root Mean Square Error (RMSE) of the registration.
+            - Optionally visualizes the registration result if the RMSE exceeds the mean error or is zero.
+        Notes:
+            - The function assumes that the incoming point cloud is in the same coordinate frame as the 
+              previously stored point cloud.
+            - The transformation matrix is inverted before updating the current pose.
+            - The function uses Open3D for point cloud processing and registration.
+        Raises:
+            None
+        """
 
-        start_time = time.time()
+        if logger.isEnabledFor(logging.DEBUG):
+            start_time = time.time()
+            self.logging_data = ""
         
         # If this is the first message, store it
         if self.actual_PCD is None:
@@ -59,73 +127,108 @@ class LocationNode (Node):
             
             # Publish the stamped pose
             self.location_publisher_.publish(stamped_pose)
+            
+            logger.info("First Message published")
 
         # If this is not the first message, compare it with the previous one
         else:
+            # Update variable for indexing the poses
+            self.index += 1
 
             # Copy of the previous step
             source_temp = self.actual_PCD
+            
             # Copy of the actual step
             target_temp = pointcloud2_to_open3d(msg)
             
             # Preprocess clouds for registration (Downsampling)
-            source_down, target_down, source_fpfh, target_fpfh = self.prepare_dataset(source_temp, target_temp, voxel_size)
-            # print(f"prepare_dataset done at {time.time() - start_time:.4f} seconds")
+            source_down, target_down, source_fpfh, target_fpfh = self.prepare_dataset(source_temp, target_temp)
+            
+            if logger.isEnabledFor(logging.DEBUG):
+                self.logging_data += "Downsampling: " + str(time.time() - start_time) + "\n"
 
             # Initial alignment (Ransac or Fast global registration)
-            initial_alignment = self.ransac_global_registration(source_down, target_down, source_fpfh, target_fpfh, voxel_size)
-            # print(f"Initial alignment done at {time.time() - start_time:.4f} seconds")
+            initial_alignment = self.ransac_global_registration(source_down, target_down, source_fpfh, target_fpfh)
             
-            # draw_registration_result(source_temp, target_temp, initial_alignment.transformation)
-            
-            """ # Downsample target point cloud before normal estimation
-            target_temp_downsampled = target_temp.voxel_down_sample(voxel_size)
-            print(f"Target downsampled at {time.time() - start_time:.4f} seconds") """
-            
-            # Compute normals for the target point cloud
-            target_temp.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=voxel_size*2, max_nn=30))
-            # print(f"Normals computed at {time.time() - start_time:.4f} seconds")
+            if logger.isEnabledFor(logging.DEBUG):
+                self.logging_data += "Initial_alingment: " + str(time.time() - start_time) + "\n"
 
             # ICP registration result using Point to Plane method
             reg_p2p = o3d.pipelines.registration.registration_icp(
-                source_temp, target_temp, threshold, initial_alignment.transformation,
+                source_down, target_down, self.THRESHOLD, initial_alignment.transformation,
                 o3d.pipelines.registration.TransformationEstimationPointToPlane())
-            # print(f"ICP registration done at {time.time() - start_time:.4f} seconds")
             
-            self.n_msg =+ 1
+            if logger.isEnabledFor(logging.DEBUG):
+                self.logging_data += "ICP: " + str(time.time() - start_time) + "\n"
             
-            if self.n_msg == 0:
-                self.mean_error = reg_p2p.inlier_rmse
-            else:
-                self.mean_error =+ (reg_p2p.inlier_rmse - self.mean_error) / self.n_msg
-
-            # if (self.mean_error < reg_p2p.inlier_rmse):
-            #     draw_registration_result(source_temp, target_temp, reg_p2p.transformation)
+            # Code for debugging
+            if logger.isEnabledFor(logging.DEBUG):
                 
-            # print("RMSE: " + str(reg_p2p.inlier_rmse) + " / Mean:" + str(self.mean_error))
-            print(str(reg_p2p.inlier_rmse) + ",")
+                self.mean_error += (reg_p2p.inlier_rmse - self.mean_error) / self.index
+                                
+                self.logging_data += "RMSE: " + str(reg_p2p.inlier_rmse) + " / Mean:" + str(self.mean_error) + "\n"
+                
+                # if (self.mean_error < reg_p2p.inlier_rmse) or (reg_p2p.inlier_rmse == 0.0):
+                #    draw_registration_result(source_down, target_down, reg_p2p.transformation)
             
             # Compute the inverse of the transformation matrix
             transformation_inv = np.linalg.inv(reg_p2p.transformation)
             
-            # Update the actual pose with the new transformation
-            self.actual_pose = np.dot(self.actual_pose, transformation_inv)
-            #print(f"Pose updated at {time.time() - start_time:.4f} seconds")
+            if reg_p2p.inlier_rmse != 0.0:
+                # Update the actual pose with the new transformation
+                self.actual_pose = np.dot(self.actual_pose, transformation_inv)
+                
+            if logger.isEnabledFor(logging.DEBUG):
+                self.logging_data += "Pose_updated: " + str(time.time() - start_time) + "\n"
+            
+                # We update the PCD for the next step
+                self.actual_PCD = target_temp
+            
+            elif logger.isEnabledFor(logging.DEBUG):
+                logger.warning("WARNING: Pose not updated due to missalignment (RMSE == 0.0)")
             
             # Convert actual pose to PoseStamped
             stamped_pose = self.matrix_to_pose_stamped(self.actual_pose)
-            #print(f"Pose published at {time.time() - start_time:.4f} seconds")
-            
+
             # Publish the stamped pose
             self.location_publisher_.publish(stamped_pose)
             
-            # We update the PCD for the next step
-            self.actual_PCD = target_temp
+            if logger.isEnabledFor(logging.DEBUG):
+                self.logging_data += "Pose_published: " + str(time.time() - start_time) + "\n"
+                
+                logger.debug("\n" + self.logging_data)
+                     
+            logger.info(f"MSG Nº {self.index} published")
             
-    def ransac_global_registration(self, source_down, target_down, source_fpfh, target_fpfh, voxel_size):
+                      
+    def ransac_global_registration(self, source_down, target_down, source_fpfh, target_fpfh, voxel_size = VOXEL_SIZE, ratio = VOXEL_REDUCTION_RATIO):
+        
+        """
+        Perform global registration of two point clouds using the RANSAC algorithm.
+        This method uses RANSAC-based feature matching to estimate the transformation
+        matrix that aligns the source point cloud to the target point cloud.
+        Args:
+            source_down (open3d.geometry.PointCloud): The downsampled source point cloud.
+            target_down (open3d.geometry.PointCloud): The downsampled target point cloud.
+            source_fpfh (open3d.pipelines.registration.Feature): The FPFH features of the source point cloud.
+            target_fpfh (open3d.pipelines.registration.Feature): The FPFH features of the target point cloud.
+            voxel_size (float, optional): The voxel size used for downsampling and determining
+                the distance threshold. Defaults to VOXEL_SIZE.
+        Returns:
+            open3d.pipelines.registration.RegistrationResult: The result of the RANSAC registration,
+            containing the estimated transformation matrix and inlier information.
+        Notes:
+            - The `distance_threshold` is set to 1 by default, which can be adjusted based on
+              the voxel size and the desired level of correspondence.
+            - The RANSAC algorithm uses a combination of correspondence checkers:
+              - Edge length checker with a threshold of 0.9.
+              - Distance checker with the specified `distance_threshold`.
+            - The RANSAC convergence criteria are set to a maximum of 100,000 iterations
+              and a confidence level of 0.999.
+        """
 
         # Cloud downsampling ratio to determine correspondance between clouds
-        distance_threshold = voxel_size * 1.5
+        distance_threshold =  voxel_size * ratio
 
         # Calculates the transformation matrix using ransac
         result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
@@ -141,44 +244,93 @@ class LocationNode (Node):
         
         return result
     
-    def execute_fast_global_registration(self, source_down, target_down, source_fpfh, target_fpfh, voxel_size):
+    
+    def execute_fast_global_registration(self, source_down, target_down, source_fpfh, target_fpfh, voxel_size = VOXEL_SIZE, ratio = VOXEL_REDUCTION_RATIO):
+        """
+        Executes the Fast Global Registration (FGR) algorithm to align two point clouds.
+        This function computes the transformation matrix that aligns the source point cloud
+        to the target point cloud using feature-based matching and the Fast Global Registration method.
+        Args:
+            source_down (open3d.geometry.PointCloud): The downsampled source point cloud.
+            target_down (open3d.geometry.PointCloud): The downsampled target point cloud.
+            source_fpfh (open3d.pipelines.registration.Feature): The FPFH features of the source point cloud.
+            target_fpfh (open3d.pipelines.registration.Feature): The FPFH features of the target point cloud.
+            voxel_size (float, optional): The voxel size used for downsampling. Defaults to VOXEL_SIZE.
+        Returns:
+            open3d.pipelines.registration.RegistrationResult: The result of the registration, 
+            including the transformation matrix and fitness score.
+        """
 
         # Cloud downsampling ratio to determine correspondance between clouds
-        distance_threshold = voxel_size * 0.5
+        distance_threshold =  voxel_size * ratio
         
         # Calculates the transformation matrix using fast global registration
         result = o3d.pipelines.registration.registration_fgr_based_on_feature_matching(
             source_down, target_down, source_fpfh, target_fpfh,
             o3d.pipelines.registration.FastGlobalRegistrationOption(
-                maximum_correspondence_distance=distance_threshold))
+                maximum_correspondence_distance = distance_threshold))
         
         return result
     
-    def prepare_dataset(self, source_pcd, target_pcd, voxel_size):
+    
+    def prepare_dataset(self, source_pcd, target_pcd, voxel_size = VOXEL_SIZE, normal_factor = NORMAL_FACTOR, feature_factor = FEATURE_FACTOR):
+        
+        """
+        Prepares the dataset for point cloud registration by preprocessing the source and target point clouds.
+        Args:
+            source_pcd (open3d.geometry.PointCloud): The source point cloud to be processed.
+            target_pcd (open3d.geometry.PointCloud): The target point cloud to be processed.
+            voxel_size (float, optional): The voxel size used for downsampling the point clouds. Defaults to VOXEL_SIZE.
+            normal_factor (float, optional): The factor used for estimating normals. Defaults to NORMAL_FACTOR.
+            feature_factor (float, optional): The factor used for computing FPFH features. Defaults to FEATUR_FACTOR.
+        Returns:
+            tuple: A tuple containing:
+                - source_down (open3d.geometry.PointCloud): The downsampled source point cloud.
+                - target_down (open3d.geometry.PointCloud): The downsampled target point cloud.
+                - source_fpfh (open3d.pipelines.registration.Feature): The FPFH features of the source point cloud.
+                - target_fpfh (open3d.pipelines.registration.Feature): The FPFH features of the target point cloud.
+        """
         
         # Preprocess pcd with given parameters
-        source_down, source_fpfh = self.preprocess_point_cloud(source_pcd, voxel_size)
-        target_down, target_fpfh = self.preprocess_point_cloud(target_pcd, voxel_size)
+        source_down, source_fpfh = self.preprocess_point_cloud(source_pcd, voxel_size, normal_factor, feature_factor)
+        target_down, target_fpfh = self.preprocess_point_cloud(target_pcd, voxel_size, normal_factor, feature_factor)
 
         return source_down, target_down, source_fpfh, target_fpfh
 
-    def preprocess_point_cloud(self, pcd, voxel_size):
+
+    def preprocess_point_cloud(self, pcd, voxel_size, normal_factor, feature_factor ):
+        
+        """
+        Preprocesses a point cloud by downsampling, estimating normals, and computing FPFH features.
+        Args:
+            pcd (open3d.geometry.PointCloud): The input point cloud to preprocess.
+            voxel_size (float): The voxel size used for downsampling the point cloud.
+            normal_factor (float): A multiplier for the voxel size to determine the radius for normal estimation.
+            feature_factor (float): A multiplier for the voxel size to determine the radius for FPFH feature computation.
+        Returns:
+            tuple: A tuple containing:
+                - pcd_down (open3d.geometry.PointCloud): The downsampled point cloud.
+                - pcd_fpfh (open3d.pipelines.registration.Feature): The computed FPFH features of the downsampled point cloud.
+        """
 
         # Donwsamples with using voxel size ratio
-        pcd_down = pcd.voxel_down_sample(voxel_size)
+        pcd_down = pcd.voxel_down_sample(voxel_size * normal_factor)
 
-        radius_normal = voxel_size * 2
+        radius_normal = voxel_size * self.NORMAL_FACTOR
+        
         # Estimate normal with radius as search criteria
         pcd_down.estimate_normals(
             o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
 
-        radius_feature = voxel_size * 5
+        radius_feature = voxel_size * feature_factor
+        
         # Coumputes Fast Point Features Histograms with selected search radius
         pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(
             pcd_down,
             o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
         
         return pcd_down, pcd_fpfh
+    
     
     def matrix_to_pose_stamped(self, matrix, frame_id="map"):
         
@@ -191,7 +343,7 @@ class LocationNode (Node):
         # Crear el mensaje PoseStamped
         pose_stamped = PoseStamped()
         pose_stamped.header.frame_id = frame_id
-        pose_stamped.header.stamp = self.get_clock().now().to_msg()
+        pose_stamped.header.stamp.sec = self.index
         
         # Asignar la traslación
         pose_stamped.pose.position.x = translation[0]
