8eb2eccc14b6317c4fe4fa2c69ebecb6589914e4
Pablo-pc <vazquezverapablo@gmail.com>
2025-09-07 11:37:54 +0200

    Deleting build files

--- project_ws/build/location_controller/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/pablo-pc/GITHUB/TFG/project_ws/install/location_controller'
--- project_ws/build/mapping_controller/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/pablo-pc/GITHUB/TFG/project_ws/install/bonxai_wrapper/share/bonxai_wrapper/package.sh"
